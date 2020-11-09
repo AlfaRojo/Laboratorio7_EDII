804a03c59e3f899dd7e5e7a81f821ecca2b9747a
AlfaRojo <38761407+AlfaRojo@users.noreply.github.com>
2020-11-08 21:49:42 -0600

    Agregando controlador rsa
    Agregando Swagger y otros cambios

---
+++ API_RSA/Controllers/rsaController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_RSA.Controllers
+{
+    /// <summary>
+    /// Cifrado Asimétrico RSA
+    /// </summary>
+    [Route("api/[controller]")]
+    public class rsaController : Controller
+    {
+        /// <summary>
+        /// Generación de llave pública y privada
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns></returns>
+        [HttpGet, Route("{p}/{q}")]
+        public ActionResult Get_Key(int p, int q)
+        {
+            int privateKey = 0;
+            int publicKey = 0;
+            return Ok($"{privateKey},{publicKey}");
+        }
+
+        /// <summary>
+        /// Recibe 
+        /// </summary>
+        /// <param name="nombre"></param>
+        /// <returns></returns>
+        [HttpPost, Route("{nombre}")]
+        public ActionResult Post_Key(string nombre)
+        {
+            return Ok();
+        }
+    }
+}
