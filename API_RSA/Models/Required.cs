13694d45c17bb3b90fa3199217dc6829739fa881
AlfaRojo <38761407+AlfaRojo@users.noreply.github.com>
2020-11-20 13:31:52 -0600

    Agregando [Required] en requisitos
    Archivos obligatorios a recibir

--- API_RSA/Models/Required.cs
+++ API_RSA/Models/Required.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 
 namespace API_RSA.Models
 {
     public class Required
     {
+        [Required]
         public IFormFile keyFile { get; set; }
+        [Required]
         public IFormFile cipherFile { get; set; }
 
     }
