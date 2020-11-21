565ad6106129763d569346d8020cadbd74c5555b
AlfaRojo <38761407+AlfaRojo@users.noreply.github.com>
2020-11-20 21:54:45 -0600

    Retornando commit

--- Laboratorio7_EDII/RSA/CipherDecipher.cs
+++ Laboratorio7_EDII/RSA/CipherDecipher.cs
@@ -7,6 +7,7 @@ namespace Lab7_EDII.RSA
 {
     public class CipherDecipher
     {
+        List<char> list_Bin = new List<char>();
         public void CifrarDescifrar(FileStream ArchivoImportado, string Llave, string newName)
         {
             ArchivoImportado.Close();
@@ -17,86 +18,129 @@ namespace Lab7_EDII.RSA
             {
                 var bufferLength = 80;
                 var buffer = new byte[bufferLength];
-                using (var Writer = new StreamWriter(Path.Combine($"RSA", newName + ".txt"), true))
+                using (var Writer = new BinaryWriter(File.OpenWrite(Path.Combine($"RSA", newName + ".txt"))))
                 {
                     using (var reader = new BinaryReader(file_Cipher))
                     {
-                        var left_over = string.Empty;
+                        var max_Length = Convert.ToString(n, 2).Length;
+
                         while (reader.BaseStream.Position != reader.BaseStream.Length)
                         {
                             buffer = reader.ReadBytes(bufferLength);
                             var buffer_write = new byte[buffer.Length];
                             foreach (var bytetxt in buffer)
                             {
-                                BigInteger new_Text = module_Text(bytetxt, key, n);
+                                var new_Text = module_Text(bytetxt, key, n);
                                 if (new_Text < 0)
                                 {
                                     new_Text += n;
                                 }
-                                var bin_Length = left_over + ToNBase(new_Text, 2);
-                                string texter = string.Empty;
-                                if (bin_Length.Length > 8)
+                                var bin_Length = Convert.ToString((byte)new_Text, 2);
+                                if (max_Length > bin_Length.Length) 
                                 {
-                                    texter = bin_Length.Substring(0, 8);
-                                    left_over = bin_Length.Replace(texter, string.Empty);
-                                    var text_ASCII = (char)Convert.ToInt32(texter, 2);
-                                    Writer.Write(text_ASCII);
+                                    bin_Length = Complete_bin(max_Length, bin_Length);
                                 }
-                                else
-                                {
-                                    var text_ASCII = ToNBase(Convert.ToInt32(bin_Length, 2), 2);
-                                    var new_text = (char)Convert.ToInt32(text_ASCII, 2);
-                                    Writer.Write(new_text);
-                                }
-                                if (left_over.Length >= 8)
-                                {
-                                    texter = left_over.Substring(0, 8);
-                                    left_over = left_over.Replace(texter, string.Empty);
-                                    var text_ASCII = (char)Convert.ToInt32(texter, 2);
-                                    Writer.Write(text_ASCII);
-                                }
+                                uint result = Convert.ToUInt32(bin_Length, 2);
+                                
+                                var text_ASCII = (char)result;
+                                list_Bin.Add(text_ASCII);
                             }
-                            if (!left_over.Equals(""))
-                            {
-                                var text_ASCII = ToNBase(Convert.ToInt32(left_over, 2), 2);
-                                var new_text = (char)Convert.ToInt32(text_ASCII, 2);
-                                Writer.Write(new_text);
-                            }
+                        }
+                        reader.ReadBytes(bufferLength);
+                        //var toWrite = Get_ASCII();
+                        foreach (var item in list_Bin)
+                        {
+                            Writer.Write(item);
                         }
                     }
                 }
             }
         }
-
-        private static string ToNBase(BigInteger a, int n)
-        {
-            StringBuilder sb = new StringBuilder();
-            while (a > 0)
-            {
-                sb.Insert(0, a % n);
-                a /= n;
-            }
-            return sb.ToString();
-        }
-        public string[] Create_Kyes(int primo1, int primo2)
+        public void CreacionLlaves(int primo1, int primo2)
         {
             int phi = (primo1 - 1) * (primo2 - 1);
             int n = primo1 * primo2;
             int e = get_E(phi, n);
             int d = modInverse(e, phi);
-            if (d < 0)
+
+            if (d < 0 )
             {
                 d += phi;
             }
             string llavePrivada = d.ToString() + "," + n.ToString();
             string llavePublica = e.ToString() + "," + n.ToString();
-            string[] keys = { llavePrivada, llavePublica };
-            return keys;
+            CreateFile(llavePrivada, "private.key");
+            CreateFile(llavePublica, "public.key");
         }
-        private BigInteger module_Text(byte text, int key, int mod)
+        public void CreateFile(string textoResultante, string tipo)
         {
-            return BigInteger.ModPow(text, key, mod);
+            using (FileStream Archivo = File.Create(@"RSA\" + tipo))
+            {
+                byte[] info = new UTF8Encoding(true).GetBytes(textoResultante);
+                Archivo.Write(info, 0, info.Length);
+                byte[] data = new byte[] { 0x0 };
+                Archivo.Write(data, 0, data.Length);
+            }
         }
+
+        private List<string> Get_ASCII()
+        {
+            List<string> vs = new List<string>();
+            var appended = string.Empty;
+            string aux = string.Empty;
+            for (int i = 0; i < list_Bin.Count(); i++)
+            {
+                if (i <= list_Bin.Count())
+                {
+                    string actual = aux + list_Bin.ElementAt(i);
+                    if (actual.Length > 7)
+                    {
+                        appended = actual.Substring(0, 8);
+                        actual = actual.Replace(appended, string.Empty);
+                    }
+                    if (actual != null)
+                    {
+                        aux = actual;
+                    }
+                    var new_text = Encoding.ASCII.GetString(GetBytes(appended));
+                    vs.Add(new_text);
+                    if (aux.Length == 8)
+                    {
+                        var new_byte = Encoding.ASCII.GetString(GetBytes(appended));
+                        vs.Add(new_byte);
+                        aux = string.Empty;
+                    }
+                }
+            }
+            return vs;
+        }
+
+        private static byte[] GetBytes(string bitString)
+        {
+            return Enumerable.Range(0, bitString.Length / 8).
+                Select(pos => Convert.ToByte(
+                    bitString.Substring(pos * 8, 8),
+                    2)
+                ).ToArray();
+        }
+
+        private string Complete_bin(int size, string actual)
+        {
+            while (size > actual.Length)
+            {
+                StringBuilder stringBuilder = new StringBuilder("0");
+                stringBuilder.Append(actual);
+                actual = stringBuilder.ToString();
+            }
+            return actual;
+        }
+
+        private long module_Text(byte text, int key, int mod)
+        {
+            long module = (long)BigInteger.ModPow(text, key, mod);
+            return module;
+        }
+
         private int modInverse(int a, int n)
         {
             int i = n, v = 0, d = 1;
@@ -117,7 +161,7 @@ namespace Lab7_EDII.RSA
         {
             var rand = new Random();
             int value = rand.Next(2, n);
-            for (int i = value; i < n + 100; i++)
+            for (int i = value; i < 10000; i++)
             {
                 if (isPrime(i))
                 {
